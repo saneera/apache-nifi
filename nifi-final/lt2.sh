aa39fda9ad33aa840096040dfe969e21b3175e11
Saneera Yapa <saneera.yapa@nuevotek.com>
2026-03-06 13:13:24 +1030

    nify

--- nifi-final/lt2.sh
+++ nifi-final/lt2.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 set -e
 
 echo "Starting NiFi Flow Deployment"
 
 ############################################
-# env
+# Required environment variables
 ############################################
 
 NIFI_URL=${NIFI_URL}
@@ -20,192 +19,205 @@ FLOW_DIR=${FLOW_DIR:-/flows}
 
 get_root_pg() {
 
-curl -s \
-"$NIFI_URL/nifi-api/flow/process-groups/root" \
-| jq -r '.processGroupFlow.id'
+  curl -k -s \
+  "$NIFI_URL/nifi-api/flow/process-groups/root" \
+  | jq -r '.processGroupFlow.id'
 }
 
 get_pg_revision() {
 
-curl -s \
-"$NIFI_URL/nifi-api/process-groups/$1" \
-| jq '.revision.version'
+  curl -k -s \
+  "$NIFI_URL/nifi-api/process-groups/$1" \
+  | jq '.revision.version'
 }
 
 find_pg() {
 
-curl -s \
-"$NIFI_URL/nifi-api/flow/process-groups/$ROOT_PG" \
-| jq -r ".processGroupFlow.flow.processGroups[]
-| select(.component.name==\"$1\")
-| .component.id"
+  curl -k -s \
+  "$NIFI_URL/nifi-api/flow/process-groups/$ROOT_PG" \
+  | jq -r ".processGroupFlow.flow.processGroups[]
+  | select(.component.name==\"$1\")
+  | .component.id"
 }
 
 create_pg() {
 
-echo "Creating process group $1"
+  echo "Creating process group $1"
 
-curl -s -X POST \
-"$NIFI_URL/nifi-api/process-groups/$ROOT_PG/process-groups" \
--H "Content-Type: application/json" \
--d "{
-\"revision\":{\"version\":0},
-\"component\":{\"name\":\"$1\"}
-}" | jq -r '.component.id'
-}
-
-is_versioned() {
-
-curl -s \
-"$NIFI_URL/nifi-api/versions/process-groups/$1" \
-| jq '.versionedFlow.flowId'
-}
-
-calculate_local_hash() {
-
-jq -S '.' "$1" | sha256sum | awk '{print $1}'
-}
-
-get_flow_id() {
-
-curl -s \
-"$NIFI_URL/nifi-api/versions/process-groups/$1" \
-| jq -r '.versionedFlow.flowId'
-}
-
-get_registry_hash() {
-
-FLOW_ID=$1
-
-LATEST=$(curl -s \
-"$REGISTRY_URL/nifi-registry-api/buckets/$BUCKET_ID/flows/$FLOW_ID/versions/latest")
-
-echo "$LATEST" \
-| jq -r '.snapshot.snapshotMetadata.comments' \
-| grep -o 'flow-hash:[a-z0-9]*' \
-| cut -d':' -f2
-}
-
-start_version_control() {
-
-PG_ID=$1
-FLOW_NAME=$2
-FILE=$3
-
-HASH=$(calculate_local_hash "$FILE")
-REVISION=$(get_pg_revision "$PG_ID")
-
-echo "Starting version control for $FLOW_NAME"
-
-curl -s -X POST \
-"$NIFI_URL/nifi-api/versions/process-groups/$PG_ID" \
--H "Content-Type: application/json" \
--d "{
-\"processGroupRevision\":{\"version\":$REVISION},
-\"versionedFlow\":{
-\"registryId\":\"$REGISTRY_ID\",
-\"bucketId\":\"$BUCKET_ID\",
-\"flowName\":\"$FLOW_NAME\",
-\"comments\":\"flow-hash:$HASH\",
-\"branch\":\"main\"
-}
-}"
-}
-
-commit_flow_if_changed() {
-
-FILE=$1
-PG_ID=$2
-FLOW_ID=$3
-
-LOCAL_HASH=$(calculate_local_hash "$FILE")
-REGISTRY_HASH=$(get_registry_hash "$FLOW_ID")
-
-echo "Local hash: $LOCAL_HASH"
-echo "Registry hash: $REGISTRY_HASH"
-
-if [ "$LOCAL_HASH" == "$REGISTRY_HASH" ]; then
-echo "No flow change detected"
-return
-fi
-
-REVISION=$(get_pg_revision "$PG_ID")
-
-echo "Flow changed, committing new version"
-
-curl -s -X POST \
-"$NIFI_URL/nifi-api/versions/process-groups/$PG_ID/commit" \
--H "Content-Type: application/json" \
--d "{
-\"processGroupRevision\":{\"version\":$REVISION},
-\"versionedFlow\":{
-\"action\":\"COMMIT\",
-\"comments\":\"flow-hash:$LOCAL_HASH\"
-}
-}"
-}
-
-update_flow() {
-
-PG_ID=$1
-
-INFO=$(curl -s \
-"$NIFI_URL/nifi-api/versions/process-groups/$PG_ID")
-
-FLOW_ID=$(echo "$INFO" | jq -r '.versionedFlow.flowId')
-
-LATEST=$(curl -s \
-"$REGISTRY_URL/nifi-registry-api/buckets/$BUCKET_ID/flows/$FLOW_ID/versions/latest" \
-| jq '.version')
-
-REVISION=$(get_pg_revision "$PG_ID")
-
-echo "Updating flow to version $LATEST"
-
-curl -s -X PUT \
-"$NIFI_URL/nifi-api/versions/process-groups/$PG_ID" \
--H "Content-Type: application/json" \
--d "{
-\"processGroupRevision\":{\"version\":$REVISION},
-\"versionedFlow\":{\"version\":$LATEST}
-}"
-}
-
-deploy_flow() {
-
-FILE=$1
-FLOW_NAME=$(basename "$FILE" .json)
-
-echo ""
-echo "Processing flow: $FLOW_NAME"
-
-PG_ID=$(find_pg "$FLOW_NAME")
-
-if [ -z "$PG_ID" ]; then
-PG_ID=$(create_pg "$FLOW_NAME")
-fi
-
-echo "Process Group ID: $PG_ID"
-
-VC=$(is_versioned "$PG_ID")
-
-if [ "$VC" == "null" ]; then
-
-start_version_control "$PG_ID" "$FLOW_NAME" "$FILE"
-
-else
-
-FLOW_ID=$(get_flow_id "$PG_ID")
-
-commit_flow_if_changed "$FILE" "$PG_ID" "$FLOW_ID"
-
-fi
-
-update_flow "$PG_ID"
+  curl -k -s -X POST \
+  "$NIFI_URL/nifi-api/process-groups/$ROOT_PG/process-groups" \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"revision\": {\"version\":0},
+    \"component\": {
+      \"name\":\"$1\"
+    }
+  }" | jq -r '.component.id'
 }
 
 ############################################
-# main
+# upload flow snapshot
+############################################
+
+upload_flow() {
+
+  PG_ID=$1
+  FILE=$2
+
+  echo "Uploading flow snapshot $FILE"
+
+  curl -k -s -X POST \
+  "$NIFI_URL/nifi-api/process-groups/$PG_ID/flow-contents?disconnectedNodeAcknowledged=true" \
+  -H "Content-Type: application/json" \
+  -d @"$FILE"
+}
+
+############################################
+# version control checks
+############################################
+
+is_versioned() {
+
+  curl -k -s \
+  "$NIFI_URL/nifi-api/versions/process-groups/$1" \
+  | jq '.versionedFlow.flowId'
+}
+
+############################################
+# start version control
+############################################
+
+start_version_control() {
+
+  PG_ID=$1
+  FLOW_NAME=$2
+  REVISION=$(get_pg_revision "$PG_ID")
+
+  echo "Starting version control for $FLOW_NAME"
+
+  curl -k -s -X POST \
+  "$NIFI_URL/nifi-api/versions/process-groups/$PG_ID" \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"processGroupRevision\": {
+      \"version\": $REVISION
+    },
+    \"versionedFlow\": {
+      \"registryId\": \"$REGISTRY_ID\",
+      \"bucketId\": \"$BUCKET_ID\",
+      \"flowName\": \"$FLOW_NAME\",
+      \"comments\": \"Initial version\",
+      \"branch\": \"main\"
+    }
+  }"
+}
+
+############################################
+# commit flow changes
+############################################
+
+commit_flow() {
+
+  PG_ID=$1
+  FLOW_NAME=$2
+
+  REVISION=$(get_pg_revision "$PG_ID")
+
+  echo "Committing changes for $FLOW_NAME"
+
+  curl -k -s -X POST \
+  "$NIFI_URL/nifi-api/versions/process-groups/$PG_ID/commit" \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"processGroupRevision\": {
+      \"version\": $REVISION
+    },
+    \"versionedFlow\": {
+      \"action\": \"COMMIT\",
+      \"comments\": \"Automated commit\"
+    }
+  }"
+}
+
+############################################
+# update flow version
+############################################
+
+update_flow() {
+
+  PG_ID=$1
+
+  INFO=$(curl -k -s \
+  "$NIFI_URL/nifi-api/versions/process-groups/$PG_ID")
+
+  FLOW_ID=$(echo "$INFO" | jq -r '.versionedFlow.flowId')
+
+  LATEST=$(curl -k -s \
+  "$REGISTRY_URL/nifi-registry-api/buckets/$BUCKET_ID/flows/$FLOW_ID/versions/latest" \
+  | jq '.version')
+
+  REVISION=$(get_pg_revision "$PG_ID")
+
+  echo "Updating flow to registry version $LATEST"
+
+  curl -k -s -X PUT \
+  "$NIFI_URL/nifi-api/versions/process-groups/$PG_ID" \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"processGroupRevision\": {
+      \"version\": $REVISION
+    },
+    \"versionedFlow\": {
+      \"version\": $LATEST
+    }
+  }"
+}
+
+############################################
+# deploy single flow
+############################################
+
+deploy_flow() {
+
+  FILE=$1
+  FLOW_NAME=$(basename "$FILE" .json)
+
+  echo ""
+  echo "Processing flow $FLOW_NAME"
+
+  PG_ID=$(find_pg "$FLOW_NAME")
+
+  if [ -z "$PG_ID" ]; then
+
+      PG_ID=$(create_pg "$FLOW_NAME")
+
+      echo "Process group created $PG_ID"
+
+      upload_flow "$PG_ID" "$FILE"
+
+  else
+
+      echo "Process group already exists: $PG_ID"
+
+  fi
+
+  VC=$(is_versioned "$PG_ID")
+
+  if [ "$VC" == "null" ]; then
+
+      start_version_control "$PG_ID" "$FLOW_NAME"
+
+  else
+
+      commit_flow "$PG_ID" "$FLOW_NAME"
+
+  fi
+
+  update_flow "$PG_ID"
+}
+
+############################################
+# start deployment
 ############################################
 
 ROOT_PG=$(get_root_pg)
@@ -214,7 +226,7 @@ echo "Root Process Group: $ROOT_PG"
 
 for flow in $FLOW_DIR/*.json
 do
-deploy_flow "$flow"
+  deploy_flow "$flow"
 done
 
 echo ""
