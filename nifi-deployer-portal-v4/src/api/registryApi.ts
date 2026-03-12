2d54a8bb0a7261eb2710cc6e0e98e3a66156a15f
Saneera Yapa <saneera.yapa@nuevotek.com>
2026-03-12 12:35:45 +1030

    nify

--- nifi-deployer-portal-v4/src/api/registryApi.ts
+++ nifi-deployer-portal-v4/src/api/registryApi.ts
@@ -21,3 +21,17 @@ export async function getLatestFlow(bucketId:string, flowId:string){
     return res.data
 
 }
+
+export async function createRegistryFlow(name:string){
+
+    const res = await axios.post(
+        `${REG_URL}/nifi-registry-api/buckets/${BUCKET}/flows`,
+        {
+            name,
+            description:"created-from-ui"
+        }
+    )
+
+    return res.data
+
+}
